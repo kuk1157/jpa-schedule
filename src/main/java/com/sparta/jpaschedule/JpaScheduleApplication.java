4d7436fd534613b47bda4dbb92bec235b3ade520
kuk1157 <kuk1938@naver.com>
2024-10-09 19:20:43 +0900

    git init

---
+++ src/main/java/com/sparta/jpaschedule/JpaScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.jpaschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaScheduleApplication.class, args);
+    }
+
+}
