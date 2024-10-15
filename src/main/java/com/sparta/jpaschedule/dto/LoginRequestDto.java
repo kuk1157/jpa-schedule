e556e20cb80834b7b286d31a524405603e66d4c6
kuk1157 <kuk1938@naver.com>
2024-10-15 15:42:30 +0900

    JWT 세팅 및 로그인

---
+++ src/main/java/com/sparta/jpaschedule/dto/LoginRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.jpaschedule.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginRequestDto {
+    private String email;
+    private String pw;
+}
